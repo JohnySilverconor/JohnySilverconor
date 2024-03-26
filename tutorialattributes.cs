1adc1414c4429115f13d5b5c6fec6f64afd9c676
JohnySilverconor <60777159+JohnySilverconor@users.noreply.github.com>
2024-03-26 03:24:18 -0300

    Add files via upload

---
+++ tutorialattributes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculator.Add(10, 20);
+            Calculator.Add(new List<int>() { 10, 20, 30, 45 });
+        }
+    }
+    public class Calculator 
+    {
+        [Obsolete("Use Add(List<int> Numbers) Method", true)]
+        public static int Add(int FirstNumber, int SecondNumber)
+        {
+            return FirstNumber + SecondNumber; ;
+
+        }
+        public static int Add(List<int> Numbers)
+        {
+            int Sum = 0;
+            foreach (int Number in Numbers)
+            {
+                Sum = Sum + Number;
+            }
+            return Sum;
+        }
+    }
+}
