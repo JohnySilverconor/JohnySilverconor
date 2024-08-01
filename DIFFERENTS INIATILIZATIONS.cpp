72999312e36c5eba0377a1481cbf19fe3d8ec4ac
JohnySilverconor <60777159+JohnySilverconor@users.noreply.github.com>
2024-07-31 22:36:17 -0400

    Add files via upload

---
+++ DIFFERENTS INIATILIZATIONS.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+int main()
+{
+    //DIFFERENTS INIATILIZATIONS FROM OLDEST
+    // TO NEWEST
+    int units_sold(0);
+    int unit_sold = 0;
+int unitsold = {0};
+int unitssold {0};
+    std::cout<<"Hello World";
+
+    return 0;
+}
